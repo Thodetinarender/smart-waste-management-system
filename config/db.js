19c0cea2fc3739cbc981aa4cfd36e8f968718dbd
THODETI NARENDER <thodetinarender51@gmail.com>
2025-07-02 18:27:54 +0530

    DB updated

--- config/db.js
+++ config/db.js
@@ -1,16 +1,28 @@
 require('dotenv').config(); // Load environment variables from .env
 const Sequelize = require('sequelize');
 
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,     // Use DB_NAME from .env
+//     process.env.DB_USER,     // Use DB_USER from .env
+//     process.env.DB_PASSWORD, // Use DB_PASSWORD from .env
+//     {
+//         dialect: 'mysql',
+//         host: process.env.DB_HOST,  // Use DB_HOST from .env (RDS Endpoint)
+//         port: process.env.DB_PORT,  // Use DB_PORT from .env (3306 for AWS RDS)
+//         logging: false
+//     }
+// );
+
+
 const sequelize = new Sequelize(
-    process.env.DB_NAME,     // Use DB_NAME from .env
-    process.env.DB_USER,     // Use DB_USER from .env
-    process.env.DB_PASSWORD, // Use DB_PASSWORD from .env
-    {
-        dialect: 'mysql',
-        host: process.env.DB_HOST,  // Use DB_HOST from .env (RDS Endpoint)
-        port: process.env.DB_PORT,  // Use DB_PORT from .env (3306 for AWS RDS)
-        logging: false
-    }
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  }
 );
 
 module.exports = sequelize;
